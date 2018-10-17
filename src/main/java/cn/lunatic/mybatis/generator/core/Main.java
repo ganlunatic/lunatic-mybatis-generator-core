2853d32d5d61ddb078c009cced488eb372a561a9
甘丰 <feng_gan@sui.com>
2018-10-17 19:32:24 +0800

    Mybatis代码生成器

--- src/main/java/cn/lunatic/mybatis/generator/core/Main.java
+++ src/main/java/cn/lunatic/mybatis/generator/core/Main.java
@@ -1,35 +1,43 @@
 package cn.lunatic.mybatis.generator.core;
 
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.mybatis.generator.api.MyBatisGenerator;
 import org.mybatis.generator.config.Configuration;
 import org.mybatis.generator.config.xml.ConfigurationParser;
 import org.mybatis.generator.internal.DefaultShellCallback;
 
-/**
- * 描述：
- *
- * @version 1.9.10
- * @作者：niexiaohui
- * @创建时间：2016年11月22日
- * @修改记录：
- * @since 1.9.10
- */
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+
+    static boolean flag = false;
 
     public static void main(String[] args) throws Exception {
         List<String> warnings = new ArrayList<String>();
-        boolean overwrite = true;
-        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
-        InputStream is = classloader.getResourceAsStream("config/generatorConfig.xml");
-        ConfigurationParser cp = new ConfigurationParser(warnings);
-        Configuration config = cp.parseConfiguration(is);
-        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
-        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
-        myBatisGenerator.generate(null);
+        File target = new File("target");
+        if (!target.exists()) {
+            target.mkdir();
+        }
+        InputStream is = null;
+        try {
+            is = new FileInputStream("generatorConfig.xml");
+            if (flag) {
+                ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+                is = classloader.getResourceAsStream("config/generatorConfig.xml");
+            }
+            ConfigurationParser cp = new ConfigurationParser(warnings);
+            Configuration config = cp.parseConfiguration(is);
+            DefaultShellCallback callback = new DefaultShellCallback(true);
+            MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+            myBatisGenerator.generate(null);
+        } finally {
+            if (is != null) {
+                is.close();
+            }
+        }
 
     }
 }
